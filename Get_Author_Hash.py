21e587c4f4e96d09c15161a7bf31e2b6c9a264ef
jieshanshan <37113186+jieshanshan@users.noreply.github.com>
2019-03-05 17:27:33 +0100

    Get author's information from each commit

---
+++ Get_Author_Hash.py
@@ -0,0 +1,64 @@
+from git import Repo
+from datetime import datetime
+from operator import itemgetter
+import time
+import os
+import urllib.request
+import zipfile
+import sys
+
+
+YEAR = 0
+WEEK = 1
+DAY = 2
+TIME = 3
+SHA = 4
+DATE = 5
+AUTHOR = 6
+
+
+repo = Repo('D:/sonarqube/projects/cassandra-dtest')
+commits = list(repo.iter_commits('master'))
+
+commits_in_week={}
+list_allweek = []
+list_eachday = []
+for each in commits:
+    each_time = datetime.fromtimestamp(each.committed_date)
+    each_date = datetime.date(each_time)
+    each_hour = datetime.time(each_time)
+    hash = each.hexsha
+    author = each.author
+
+    list_each = [each_time.isocalendar()[YEAR], each_time.isocalendar()[WEEK], each_time.isocalendar()[DAY], each_hour, hash, each_date, author]
+    list_allweek.append(list_each)
+
+list_allweek.sort(key=itemgetter(YEAR, WEEK, DAY, TIME))
+len_list_allweek = len(list_allweek)
+
+list_eachday.sort(reverse=False)
+for each in list_eachday:
+    print(each)
+
+i = 0
+
+
+while i + 1 < len_list_allweek:
+    if ((list_allweek[i][YEAR] == list_allweek[i + 1][YEAR]) and (list_allweek[i][WEEK] == list_allweek[i + 1][WEEK])):
+        list_allweek.pop(i)
+        len_list_allweek -= 1
+    else:
+        i += 1
+
+
+
+for each in list_allweek:
+
+    print(each)
+    commit_hash = str(each[SHA])
+    commit_author = str(each[AUTHOR])
+
+    print(commit_author)
+
+    print(commit_hash,commit_author)
+
